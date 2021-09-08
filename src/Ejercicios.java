8698749ea3336de013f6de4d20d615a2619f4e15
Zuazo1718 <jezuma1718@gmail.com>
2021-09-08 10:36:00 -0400

    Se realizo el ejercicio y el resultado salio 4

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -11,4 +11,53 @@ public class Ejercicios {
 
         return  factorial;
     }
+
+    public static int mcd(int a, int b){
+        int mcd;
+        if(a == b){
+            //clase base
+            mcd = b;
+        }else{
+            if (a > b){
+                a = a -b;
+            }else {
+                b = b - a;
+            }
+
+            mcd = mcd(a, b);
+        }
+        return mcd;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Ejercicios.factorial(5));
+        //System.out.println(Ejercicios.factorial(5));
+        System.out.println(Ejercicios.mcd(412,184));
     }
 }
