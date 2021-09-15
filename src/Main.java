389275e7c4f1aab65f5c079af15ff314cf24f44b
unknown <zambranafabrizio@gmail.com>
2021-09-15 08:36:24 -0400

    Se realizo Suma de vectorescon recursividad

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //System.out.println(Ejercicios.factorial(5));
-        System.out.println(Ejercicios.mcd(412,184));
+        //System.out.println(Ejercicios.mcd(412,184));
+        int[] vector = {4, 4, 6};
+        System.out.println(Ejercicios.sumaVector(vector,0));
     }
 }
